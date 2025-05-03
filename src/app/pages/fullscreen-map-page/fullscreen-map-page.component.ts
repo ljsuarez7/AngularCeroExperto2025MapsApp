11a5d5fec9165ecc540aa6ef7864efe1f6f5e1c8
JaviSrz <ljsuarez7@gmail.com>
2025-05-03 09:53:46 +0200

    Mostrar mapa a pantalla completa

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { environment } from '../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
   imports: [],
   templateUrl: './fullscreen-map-page.component.html',
+  styles: `
+
+  div {
+    width: 100vw;
+    height: calc(100vh - 64px);
+  }
+
+  `
 })
-export class FullscreenMapPageComponent { }
+export class FullscreenMapPageComponent implements AfterViewInit{
+
+  divElement = viewChild<ElementRef>('map');
+
+  async ngAfterViewInit() {
+
+    if(!this.divElement()?.nativeElement) return;
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()?.nativeElement;
+
+    console.log(element);
+
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+  }
+
+}
